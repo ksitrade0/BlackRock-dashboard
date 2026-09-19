a0ecfd9b436e66d6641aac24ae4aebc9ad24a558
ksitrade0 <ksitrade0@gmail.com>
2026-09-19 06:02:37 +0600

    update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  user: 'ruha_app-admin',
+  password: '2144Jannat@',
+  database: 'ruha_inventory',
 });
 
 export async function query(sql: string, values: any[] = []) {
